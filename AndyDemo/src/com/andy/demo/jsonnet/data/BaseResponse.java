45cd35c569e8b061eccef4755fc9041feff6ca93
IvanKuo <chanven0127@gmail.com>
2014-11-04 11:00:41 +0800

    json包更名

---
+++ AndyDemo/src/com/andy/demo/jsonnet/data/BaseResponse.java
@@ -0,0 +1,8 @@
+package com.andy.demo.jsonnet.data;
+
+import java.io.Serializable;
+
+public class BaseResponse implements Serializable{
+    public String message;
+    public int status;
+}
---
+++ AndyDemo/src/com/andy/demo/jsonnet/data/KuaidiInfo.java
@@ -0,0 +1,8 @@
+package com.andy.demo.jsonnet.data;
+
+public class KuaidiInfo extends BaseResponse{
+	public String ischeck;
+	public String com;
+	public String nu;
+	public String state;
+}
